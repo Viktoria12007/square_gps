b9b3084917f6973b23d216f1a9592e4f1f1ad1e8
Виктория Голубова <v.golubova@denexy.com>
2023-09-24 11:26:23 +0400

    Creating the App component

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import Header from "@/components/Header";
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+
+  data: () => ({
+    //
+  }),
+  async mounted() {
+    // this.postMarkers(null, true);
+    await this.getMarkers();
+  },
+  methods: {
+    ...mapActions("markers", ["getMarkers", "postMarkers"]),
+  },
+};
+</script>
