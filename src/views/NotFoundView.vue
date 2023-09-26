a047d1284386831ce6f3de7088c8d4f94e98b9da
Виктория Голубова <v.golubova@denexy.com>
2023-09-26 23:33:14 +0400

    The NotFoundView.vue page has been created

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="fill-height main-container">
+    <v-alert prominent type="error">
+      <v-row align="center">
+        <v-col class="grow">{{ lan("errorPageText") }}</v-col>
+        <v-col class="shrink">
+          <v-btn to="/">{{ lan("errorPageButton") }}</v-btn>
+        </v-col>
+      </v-row>
+    </v-alert>
+  </v-container>
+</template>
+
+<script>
+import languages from "@/mixins/languages";
+
+export default {
+  name: "NotFoundView",
+  components: {},
+  mixins: [languages],
+  data: () => ({}),
+};
+</script>
+
+<style lang="stylus">
+.main-container
+  justify-content: center
+  max-width: 1785px
+</style>
