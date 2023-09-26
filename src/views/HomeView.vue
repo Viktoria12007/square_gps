190db0dddf37d908f2581f8a821a348731dd0e7e
Виктория Голубова <v.golubova@denexy.com>
2023-09-26 23:30:45 +0400

    The structure of the NomeView component has been changed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,30 @@
 <template>
-  <v-container class="fill-height main-container" fluid>
-    <keep-alive>
-      <router-view />
-    </keep-alive>
-  </v-container>
+  <div class="v-main__wrap d-flex">
+    <Header />
+    <v-main>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+      <Notifications />
+    </v-main>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Notifications from "@/components/Notifications";
 export default {
   name: "HomeView",
-  components: {},
+  components: {
+    Header,
+    Notifications,
+  },
 };
 </script>
 
-<style lang="stylus" scoped>
-.container.main-container {
-  align-items: flex-start;
-  padding: 0;
-}
+<style lang="stylus">
+.main-container
+  //align-items: flex-start
+  max-width: 1785px
+  padding: 0
 </style>
