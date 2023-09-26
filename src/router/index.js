2118dd64d6cbba89357dba71527693e0c94f8c4e
Виктория Голубова <v.golubova@denexy.com>
2023-09-26 23:31:48 +0400

    Added router

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,30 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import MapView from "../views/MapView.vue";
 import AboutView from "../views/AboutView";
+import NotFoundView from "@/views/NotFoundView";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "",
     name: "Home",
     component: () => import("../views/HomeView.vue"),
 
     children: [
+      { path: "/", component: AboutView },
+
       { path: "/about", component: AboutView },
 
       { path: "/map", component: MapView },
 
-      { path: "/map/:id", component: MapView, props: true },
+      { path: "/map/:id", component: MapView },
     ],
+  },
+  {
+    path: "/:catchAll(.*)",
+    component: NotFoundView,
+    name: "NotFound",
   },
 ];
 
