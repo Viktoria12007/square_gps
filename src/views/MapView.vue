88c576ff1af65d9e50df3b748087528315d4c93f
Виктория Голубова <v.golubova@denexy.com>
2023-09-27 23:40:24 +0400

    Getting rid of v-list-item-group in favor of link and input-value props

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-container class="fill-height main-container">
+  <v-container class="fill-height map-container">
     <v-row no-gutters class="fill-height">
       <v-col cols="3">
-        <v-navigation-drawer permanent width="100%" class="main-sidebar">
+        <v-navigation-drawer permanent width="100%" class="map-sidebar">
           <v-list-item>
             <v-list-item-content>
               <v-list-item-title class="text-h6">
@@ -11,35 +11,34 @@
             </v-list-item-content>
           </v-list-item>
           <v-list v-if="markers.length">
-            <v-list-item-group :value="selectedMarkerId" mandatory>
-              <v-virtual-scroll
-                :bench="5"
-                :items="markers"
-                style="height: calc(100vh - 180px)"
-                item-height="81"
-              >
-                <template v-slot:default="{ item }">
-                  <v-list-item
-                    :key="item.id"
-                    :value="item.id"
-                    link
-                    @click="handleSelectMarker(item.id)"
-                  >
-                    <v-list-item-content>
-                      <v-list-item-title>
-                        {{ lan("markerName") + item.id }}
-                      </v-list-item-title>
-                      <v-list-item-subtitle>
-                        {{ item.position | formatedPosition }}
-                      </v-list-item-subtitle>
-                      <v-list-item-subtitle class="text--primary">
-                        {{ item.address }}
-                      </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                </template>
-              </v-virtual-scroll>
-            </v-list-item-group>
+            <v-virtual-scroll
+              :bench="0"
+              :items="markers"
+              style="height: calc(100vh - 180px)"
+              item-height="81"
+            >
+              <template v-slot:default="{ item }">
+                <v-list-item
+                  :key="item.id"
+                  :input-value="selectedMarkerId === item.id"
+                  link
+                  :to="`/map/${item.id}`"
+                  @click="handleSelectMarker(item.id)"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ lan("markerName") + item.id }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                      {{ item.position | formatedPosition }}
+                    </v-list-item-subtitle>
+                    <v-list-item-subtitle class="text--primary">
+                      {{ item.address }}
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+            </v-virtual-scroll>
           </v-list>
         </v-navigation-drawer>
       </v-col>
@@ -53,7 +52,7 @@
 <script>
 import languages from "@/mixins/languages";
 import Map from "@/components/Map";
-import { mapActions, mapGetters, mapState } from "vuex";
+import { mapState } from "vuex";
 import handleSelectMarker from "@/mixins/handleSelectMarker";
 
 export default {
@@ -73,36 +72,18 @@ export default {
   async mounted() {
     await this.getMarkers();
   },
-  // watch: {
-  //   selectedMarkerId(n) {
-  //     // eslint-disable-next-line
-  //     this.$router.push(`/map/${n}`).catch((err) => {});
-  //     const coords = [
-  //       this.getSelectedMarker.position.lat,
-  //       this.getSelectedMarker.position.lng,
-  //     ];
-  //     this.setCenter(coords);
-  //   },
-  // },
   computed: {
     ...mapState("markers", ["markers"]),
-    ...mapGetters("markers", ["getSelectedMarker"]),
-    // selectedMarkerId: {
-    //   get() {
-    //     return this.$store.state["markers/selectedMarkerId"];
-    //   },
-    //   set(value) {
-    //     this.$store.commit("markers/selectMarker", value);
-    //   },
-    // },
   },
-  methods: {
-    ...mapActions("markers", ["getMarkers"]),
-  },
+  methods: {},
 };
 </script>
 
 <style lang="stylus" scoped>
-//.main-sidebar > .v-navigation-drawer__content
+//.map-sidebar > .v-navigation-drawer__content
 //  owerflow: visible
+.map-container
+  //align-items: flex-start
+  max-width: 1785px
+  padding: 0
 </style>
