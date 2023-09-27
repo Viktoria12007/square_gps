5d90c37ea3ec5a4f277787a2f2c7b65dde1a7a09
Виктория Голубова <v.golubova@denexy.com>
2023-09-27 23:46:32 +0400

    The route change was taken out of the method handleSelectMarker

--- src/mixins/handleSelectMarker.js
+++ src/mixins/handleSelectMarker.js
@@ -1,17 +1,15 @@
-import { mapMutations, mapState } from "vuex";
+import { mapGetters, mapMutations, mapState } from "vuex";
 
 export default {
   computed: {
     ...mapState("markers", ["selectedMarkerId"]),
+    ...mapGetters("markers", ["getSelectedMarker"]),
   },
   methods: {
     ...mapMutations("preferences", ["setCenter"]),
     ...mapMutations("markers", ["selectMarker"]),
     handleSelectMarker(selectedMarkerId) {
-      console.debug("handleSelectMarker");
       this.selectMarker(selectedMarkerId);
-      // eslint-disable-next-line
-      this.$router.push(`/map/${selectedMarkerId}`).catch((err) => {});
       const coords = [
         this.getSelectedMarker.position.lat,
         this.getSelectedMarker.position.lng,
